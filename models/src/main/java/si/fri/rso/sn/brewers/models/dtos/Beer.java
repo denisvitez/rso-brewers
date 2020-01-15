01efe67547a5bf1c5f38e8d78557af091dc314dc
Denis Vitez <denis.vitez@gmail.com>
2020-01-15 21:41:48 +0100

    Extended DTO model.

--- models/src/main/java/si/fri/rso/sn/brewers/models/dtos/Beer.java
+++ models/src/main/java/si/fri/rso/sn/brewers/models/dtos/Beer.java
@@ -1,8 +1,23 @@
 package si.fri.rso.sn.brewers.models.dtos;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
 public class Beer {
     private Integer id;
+
+    private double alcohol;
+
+    private String style;
+
+    private int breweryId;
+
     private String name;
+
+    private Instant dateInserted;
 
     public Integer getId() {
         return id;
@@ -12,11 +27,43 @@ public class Beer {
         this.id = id;
     }
 
+    public double getAlcohol() {
+        return alcohol;
+    }
+
+    public void setAlcohol(double alcohol) {
+        this.alcohol = alcohol;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public int getBreweryId() {
+        return breweryId;
+    }
+
+    public void setBreweryId(int breweryId) {
+        this.breweryId = breweryId;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getDateInserted() {
+        return dateInserted;
+    }
+
+    public void setDateInserted(Instant dateInserted) {
+        this.dateInserted = dateInserted;
     }
 }
